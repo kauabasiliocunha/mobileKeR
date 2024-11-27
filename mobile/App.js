6c3ab339e4fad966540cac2930fc0987d97fddee
Kauafelippe <kauafelipao@gmail.com>
2024-11-27 13:22:47 -0300

    d

---
+++ mobile/App.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, FlatList } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
