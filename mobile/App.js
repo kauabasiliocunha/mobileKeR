81abb86f618397a7ec129a3a33f97698d68e6637
Kauafelippe <kauafelipao@gmail.com>
2024-11-27 13:23:03 -0300

    d

--- mobile/App.js
+++ mobile/App.js
@@ -2,3 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, FlatList } from 'react-native';
 import * as Location from 'expo-location';
 import axios from 'axios';
+
+
+
+export default function App() {
+    const [pais, setPais] = useState("");
+    const [tela, setTela] = useState("inicial");
+    const [perguntas, setPerguntas] = useState([]);
+    const [respostas, setRespostas] = useState({});
+    const [resultado, setResultado] = useState(null);
+    const [perguntasErradas, setPerguntasErradas] = useState([]);
